efffda9ee74b2b7944c9627a43a07d3c170959fd
JoaoWudarski <joaowudarski@gmail.com>
2021-11-21 19:24:10 -0300

    Criação de componentes no App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-system-bar app>
+      <v-row justify="center">
+        <strong> v-system-bar </strong>
+      </v-row>
+    </v-system-bar>
+    
+    <v-app-bar app>
+      <v-row justify="center">
+        <strong> v-app-bar </strong>
+      </v-row>
+    </v-app-bar>
+
+    <v-card height="86vh">
+      <v-navigation-drawer absolute permanent left>
+        <v-container fill-height>
+          <v-card-text>
+            <center>
+              <strong> v-navigation-drawer </strong>
+            </center>
+          </v-card-text>
+        </v-container>
+      </v-navigation-drawer>
+
+      <v-container fill-height>
+        <v-card-text>
+          <center>
+            <strong> v-content </strong>
+          </center>
+        </v-card-text>
+      </v-container>
+    </v-card>
+
+    <v-footer height="35px" padless>
+      <v-row class="text-center" justify="center">
+        <strong> v-footer </strong>
+      </v-row>
+    </v-footer>
+    
+    <v-bottom-navigation horizontal align="center" justify="center">
+      <v-card-text>
+        <strong> v-bottom-navigation </strong>
+      </v-card-text>
+    </v-bottom-navigation>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<script>
+  export default {
+    data: () => ({
+      links: [],
+    }),
+  }
+</script>
